cb7fc26c80bbf0395dbfe26ede5e0a2ac0fe383b
Dmitry Shmidt <dimitrysh@google.com>
2010-10-07 14:39:16 -0700

    mmc: Add "ignore mmc pm notify" functionality
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -270,6 +270,8 @@ int mmc_of_parse(struct mmc_host *host)
 	if (ro_cap_invert ^ ro_gpio_invert)
 		host->caps2 |= MMC_CAP2_RO_ACTIVE_HIGH;
 
+	if (device_property_read_bool(dev, "sd-ignore-pm-notify"))
+		host->pm_flags |= MMC_PM_IGNORE_PM_NOTIFY;
 	if (device_property_read_bool(dev, "cap-sd-highspeed"))
 		host->caps |= MMC_CAP_SD_HIGHSPEED;
 	if (device_property_read_bool(dev, "cap-mmc-highspeed"))
@@ -423,7 +425,8 @@ int mmc_add_host(struct mmc_host *host)
 #endif
 
 	mmc_start_host(host);
-	mmc_register_pm_notifier(host);
+	if (!(host->pm_flags & MMC_PM_IGNORE_PM_NOTIFY))
+		mmc_register_pm_notifier(host);
 
 	return 0;
 }
@@ -440,7 +443,9 @@ EXPORT_SYMBOL(mmc_add_host);
  */
 void mmc_remove_host(struct mmc_host *host)
 {
-	mmc_unregister_pm_notifier(host);
+	if (!(host->pm_flags & MMC_PM_IGNORE_PM_NOTIFY))
+		mmc_unregister_pm_notifier(host);
+
 	mmc_stop_host(host);
 
 #ifdef CONFIG_DEBUG_FS
