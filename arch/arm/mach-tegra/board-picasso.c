1624ef015f55a9f95a8106d4ce736a1a34eee319
Dmitry Osipenko <digetx@gmail.com>
2015-10-01 18:28:28 +0300

    ARM: tegra: picasso: Remove Wi-Fi rfkill knobs
    There is no facility to reset Wi-Fi SDIO properly in kernel. Bootloader should
    do a proper reset, so rely on it.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- arch/arm/mach-tegra/board-picasso.c
+++ arch/arm/mach-tegra/board-picasso.c
@@ -20,27 +20,6 @@
 #include <linux/platform_device.h>
 #include <linux/rfkill-gpio.h>
 
-static struct rfkill_gpio_platform_data wifi_rfkill_platform_data = {
-	.name	= "wifi_rfkill",
-	.type	= RFKILL_TYPE_WLAN,
-};
-
-static struct platform_device wifi_rfkill_device = {
-	.name	= "rfkill_gpio",
-	.id	= 0,
-	.dev	= {
-		.platform_data = &wifi_rfkill_platform_data,
-	},
-};
-
-static struct gpiod_lookup_table wifi_gpio_lookup = {
-	.dev_id = "rfkill_gpio.0",
-	.table = {
-		GPIO_LOOKUP("tegra-gpio", 86, "reset", 0),
-		{ },
-	},
-};
-
 static struct rfkill_gpio_platform_data bluetooth_rfkill_platform_data = {
 	.name	= "bluetooth_rfkill",
 	.type	= RFKILL_TYPE_BLUETOOTH,
@@ -64,9 +43,6 @@ static struct gpiod_lookup_table bluetooth_gpio_lookup = {
 
 void __init tegra_picasso_rfkill_init(void)
 {
-	gpiod_add_lookup_table(&wifi_gpio_lookup);
 	gpiod_add_lookup_table(&bluetooth_gpio_lookup);
-
-	platform_device_register(&wifi_rfkill_device);
 	platform_device_register(&bluetooth_rfkill_device);
 }
