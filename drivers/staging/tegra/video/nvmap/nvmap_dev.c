f6786cc48a1f412bafec8650a3863ec9a60a513e
Dmitry Osipenko <digetx@gmail.com>
2015-12-15 02:27:59 +0300

    staging: tegra: video: Fix LOCKDEP complains
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- drivers/staging/tegra/video/nvmap/nvmap_dev.c
+++ drivers/staging/tegra/video/nvmap/nvmap_dev.c
@@ -334,7 +334,6 @@ void nvmap_carveout_commit_add(struct nvmap_client *client,
 {
 	unsigned long flags;
 
-	nvmap_ref_lock(client);
 	spin_lock_irqsave(&node->clients_lock, flags);
 	BUG_ON(list_empty(&client->carveout_commit[node->index].list) &&
 	       client->carveout_commit[node->index].commit != 0);
@@ -347,7 +346,6 @@ void nvmap_carveout_commit_add(struct nvmap_client *client,
 			 &node->clients);
 	}
 	spin_unlock_irqrestore(&node->clients_lock, flags);
-	nvmap_ref_unlock(client);
 }
 
 void nvmap_carveout_commit_subtract(struct nvmap_client *client,
