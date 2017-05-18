2e70d7bac1431a52504fc9e32bf7266ce701c277
Dmitry Osipenko <digetx@gmail.com>
2017-05-18 17:53:12 +0300

    gpu: host1x: Forbid unrelated SETCLASS opcode in the firewall
    Several channels could be made to write the same unit concurrently via the
    SETCLASS opcode, trusting userspace is a bad idea. It should be possible to
    drop the per-client channel reservation and add a per-unit locking by
    inserting MLOCK's to the command stream to re-allow the SETCLASS opcode, but
    it will be much more work. Let's forbid the unit-unrelated class changes for
    now.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
    Reviewed-by: Erik Faye-Lund <kusmabite@gmail.com>
    Reviewed-by: Mikko Perttunen <mperttunen@nvidia.com>

--- drivers/gpu/host1x/job.c
+++ drivers/gpu/host1x/job.c
@@ -373,6 +373,9 @@ struct host1x_firewall {
 
 static int check_register(struct host1x_firewall *fw, unsigned long offset)
 {
+	if (!fw->job->is_addr_reg)
+		return 0;
+
 	if (fw->job->is_addr_reg(fw->dev, fw->class, offset)) {
 		if (!fw->num_relocs)
 			return -EINVAL;
@@ -385,6 +388,19 @@ static int check_register(struct host1x_firewall *fw, unsigned long offset)
 
 		fw->num_relocs--;
 		fw->reloc++;
+	}
+
+	return 0;
+}
+
+static int check_class(struct host1x_firewall *fw, u32 class)
+{
+	if (!fw->job->is_valid_class) {
+		if (fw->class != class)
+			return -EINVAL;
+	} else {
+		if (!fw->job->is_valid_class(fw->class))
+			return -EINVAL;
 	}
 
 	return 0;
@@ -463,10 +479,8 @@ static int validate(struct host1x_firewall *fw, struct host1x_job_gather *g)
 {
 	u32 *cmdbuf_base = (u32 *)fw->job->gather_copy_mapped +
 		(g->offset / sizeof(u32));
+	u32 job_class = fw->class;
 	int err = 0;
-
-	if (!fw->job->is_addr_reg)
-		return 0;
 
 	fw->words = g->words;
 	fw->cmdbuf = g->bo;
@@ -487,7 +501,9 @@ static int validate(struct host1x_firewall *fw, struct host1x_job_gather *g)
 			fw->class = word >> 6 & 0x3ff;
 			fw->mask = word & 0x3f;
 			fw->reg = word >> 16 & 0xfff;
-			err = check_mask(fw);
+			err = check_class(fw, job_class);
+			if (!err)
+				err = check_mask(fw);
 			if (err)
 				goto out;
 			break;
